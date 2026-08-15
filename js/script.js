6f97f4a296981f5dc5150d2f1d046130d6a96988
Fatih Sarılı <fatih.sar@icloud.com>
2026-08-15 09:44:38 +0300

    add javascript entry

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector(".navbar");
+
+console.log(navbar);
+
+const button = document.querySelector(".getoffer-button");
+
+console.log(button);
+
+button.addEventListener("click", function() {
+
+    alert("Teklif alma sayfasi yakinda eklenecek");
+
+});
